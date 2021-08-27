c7f80873b59c66952962395a634aad7dad8ace50
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-27 19:47:42 -0300

    Documentation

--- monitoring.sh
+++ monitoring.sh
@@ -4,12 +4,20 @@
 ARCH=$(uname -snrvmo)
 CPU=$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
 VCPU=$(grep 'processor' /proc/cpuinfo | uniq | wc -l)
-FREERAM=$(free -m | grep Mem: | awk '{print $3}')
-USEDRAM=$(free -m | grep Mem: | awk '{print $2}')
+FULLRAM=$(free -m | grep Mem: | awk '{print $2}')
+USEDRAM=$(free -m | grep Mem: | awk '{print $3}')
 PCTRAM=	$(free -m | grep Mem: | awk '{printf("%.2f"), $3/$2*100}')
-FREEDISK=
-USEDDISK=
-PCTDISK=
+FULLDISK=$(df -Bg | grep /dev/ | grep -v /boot | awk '{fd += $2} END {print fd}')
+USEDDISK=$(df -Bm | grep /dev/ | grep -v /boot | awk '{ud += $3} END {print ud}')
+PCTDISK=$(df -Bm | grep /dev/ | grep -v /boot | awk '{ud += $3} {fd += $2} END {printf("%d"), ud/fd*100}')
+CPU=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+LASTBOOT=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+LVM=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+TCP=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+USERLOG=$(users | wc -w)
+IP=$(hostname -I)
+MAC=$(ip link show | awk '$1 == "link/ether" {print $2}')
+SUDO=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 ## Shows the architecture of the operating system and its kernel version
 echo "#Architecture: ${ARCH}"
@@ -21,4 +29,28 @@ echo "#CPU physical: ${CPU}"
 echo "#vCPU: ${VCPU}"
 
 ## Shows the current available RAM on your server and its utilization rate as percentage
-echo "#Memory Usage: ${FREERAM}/${USEDRAM}MB (${PCTRAM}%)"
+echo "#Memory Usage: ${USEDRAM}/${FULLRAM}MB (${PCTRAM}%)"
+
+## Shows the current available memory on your server and its utilization rate as a percentage
+echo "#Disk Usage: ${USEDDISK}/${FULLDISK}Gb (${PCTDISK}%)"
+
+## Shows the current utilization rate of your processors as a percentage
+echo "#CPU load: ${CPU}"
+
+## Shows the date and time of the last reboot
+echo "#Last boot: ${LASTBOOT}"
+
+## Shows whether LVM is active or not
+echo "#LVM use: ${LVM}"
+
+## Shows the number of active connections
+echo "#Connexions TCP: ${TCP}"
+
+## Shows the number of users using the server
+echo "#User log: ${USERLOG}"
+
+## Shows the IPv4 address of your server and its MAC (Media Access Control) address
+echo "#Network: ${IP}"
+
+## Shows the number of commands executed with the sudo program
+echo "#Sudo: ${SUDO}"
